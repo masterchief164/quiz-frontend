c867e8f8b9f6857146e334ed629a402c51d2d470
masterchief164 <rishugupta2001.rg@gmail.com>
2021-06-26 16:33:11 +0530

    landing done

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,19 +3,24 @@ import React from "react";
 
 function Landing() {
     return <div className="test display-2">
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-lg-6">
+                    <div className="hey">
         <span>
             Get Ready<br/> To Compete!
         </span>
+                    </div>
+                    <div className="random">
+                        <button className="btn btn-lg bg-dark">Register</button>
+                        <button className="btn btn-lg bg-dark">Login</button>
+                    </div>
                 </div>
-                <div className="col-md-6">
+                <div className="col-lg-6">
                     <div className="image"/>
                 </div>
             </div>
         </div>
-        <button className="btn btn-lg bg-dark">Register</button>
     </div>
 }
 
