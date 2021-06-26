3987d998ca0aebd30300f8f69f57100a56157cc7
masterchief164 <rishugupta2001.rg@gmail.com>
2021-06-26 17:23:53 +0530

    landing done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ function Header() {
         console.log("clicked")
     }
     return <Navbar fixed="top" className="scale dark-theme" variant="dark" expand="lg" >
-        <Navbar.Brand onClick={toggle} href="#home"><span className="scaleText">Quizzer</span></Navbar.Brand>
+        <Navbar.Brand onClick={toggle} href="#home"><span className="scaleTitle">Quizzer</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link className="scaleText" href="#home">Home</Nav.Link>
-                <Nav.Link className="scaleText" href="#link">Link</Nav.Link>
+                <Nav.Link className="scaleText" href="#home">Contests</Nav.Link>
+                <Nav.Link className="scaleText" href="#link">Leaderboards</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 
 function Landing() {
-    return <div className="test display-2">
-        <div className="container-fluid">
+    return <div className="test">
+        <div className="container-fluid back">
             <div className="row">
-                <div className="col-lg-6">
-                    <div className="hey">
-        <span>
-            Get Ready<br/> To Compete!
-        </span>
+                <div className="col-lg-5">
+                    <h1 className="display-2">Get Ready To Compete!</h1>
+                    <div className="des">
+                        <span>The best website for participating and hosting quizzing competitions</span>
                     </div>
-                    <div className="random">
+                    <div className="but">
                         <button className="btn btn-lg bg-dark">Register</button>
                         <button className="btn btn-lg bg-dark">Login</button>
                     </div>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-5">
                     <div className="image"/>
+                    <div className="des">Wanna host a new quiz?</div>
                 </div>
             </div>
         </div>
