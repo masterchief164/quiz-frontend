6da50883d53ede4cea8c25044aeadbbfa2175893
masterchief164 <rishugupta2001.rg@gmail.com>
2021-06-26 16:08:58 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Landing from "./Landing";
+
+function App() {
+    return<div>
+        <Header/>
+        <Landing/>
+    </div>
+}
+
+export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Landing() {
+    return <div className="test display-2">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+        <span>
+            Get Ready<br/> To Compete!
+        </span>
+                </div>
+                <div className="col-md-6">
+                    <div className="image"/>
+                </div>
+            </div>
+        </div>
+        <button className="btn btn-lg bg-dark">Register</button>
+    </div>
+}
+
+export default Landing;
